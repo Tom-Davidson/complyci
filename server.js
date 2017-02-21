07a74e9e7248e806811fef070d289080c1aea47d
Tom Davidson <tom@davidson.me.uk>
2017-02-21 16:18:05 +0000

    basic koa app with 1 sort-of-test

---
+++ server.js
@@ -0,0 +1,30 @@
+const koa = require('koa');
+const parse = require('co-body');
+
+const app = koa();
+
+// x-response-time
+app.use(function* headerResponseTime(next) {
+  const start = new Date();
+  yield next;
+  const ms = new Date() - start;
+  this.set('X-Response-Time', `${ms}ms`);
+});
+// logger
+app.use(function* logger(next) {
+  const start = new Date();
+  yield next;
+  const ms = new Date() - start;
+  console.log('%s %s - %s', this.method, this.url, ms); // eslint-disable-line no-console
+});
+
+app.use(function* complyci() {
+  if (this.method === 'POST') {
+    const body = yield parse(this, { limit: '10kb' });
+    this.body = `Push to ${body.repository.full_name}`;
+  } else {
+    this.body = 'This endpoint only supports POST';
+  }
+});
+
+app.listen(3000);
