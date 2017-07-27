387a54442651be875754a77f101ff93132b3a6e2
Tom Davidson <tom@davidson.me.uk>
2017-07-27 21:01:33 +0100

    breaking it apart

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const koa = require('koa');
 const parse = require('co-body');
-const git = require('simple-git')('./repos');
-const licenseChecker = require('licenses');
+const git = require('./lib/git');
+const licenses = require('./lib/licenses');
+const reporter = require('./lib/reporters').stdout;
 require('dotenv').config();
 
 // Set up the envoronment
@@ -18,39 +19,6 @@ if (process.env.GITHUB_PRIVATE_KEY !== undefined) {
 }
 
 const app = koa();
-function licensesUpdate(project, hash, licenses) {
-  console.log(`Project: ${project}`); // eslint-disable-line no-console
-  console.log(`Commit: ${hash}`); // eslint-disable-line no-console
-  console.log(`Licenses: ${JSON.stringify(licenses)}`); // eslint-disable-line no-console
-}
-function repoChanged(repo, hash) {
-  if (fs.existsSync(`repos/${repo}/package.json`)) {
-    const licenseChecks = [];
-    const packageJSON = JSON.parse(fs.readFileSync(`repos/${repo}/package.json`));
-    Object.keys(packageJSON.dependencies).forEach((dependency) => {
-      licenseChecks.push(new Promise((resolve, reject) => {
-        licenseChecker(
-          dependency,
-          { registry: 'https://registry.npmjs.org/' },
-          function recordDepLic(err, license) { // eslint-disable-line prefer-arrow-callback
-            if (err === undefined) {
-              resolve({ dep: dependency, lic: license.join(',') });
-            } else {
-              reject({ dep: dependency, lic: 'Unknown' });
-            }
-          }.bind({ licenseChecks, dependency }) // eslint-disable-line no-extra-bind, comma-dangle
-        );
-      }));
-    });
-    Promise.all(licenseChecks).then((licenseCheckResults) => {
-      const licenses = {};
-      licenseCheckResults.forEach((licenseCheckResult) => {
-        licenses[licenseCheckResult.dep] = licenseCheckResult.lic;
-      });
-      licensesUpdate(repo, hash, licenses);
-    });
-  }
-}
 
 // x-response-time
 app.use(function* headerResponseTime(next) {
@@ -70,22 +38,10 @@ app.use(function* logger(next) {
 app.use(function* complyci() {
   if (this.method === 'POST') {
     const body = yield parse(this, { limit: '10kb' });
-    this.body = `Push to ${body.repository.full_name}`;
-    if (!fs.existsSync(`repos/${body.repository.full_name}`)) {
-      git.clone(
-        `git@github.com:${body.repository.full_name}.git`,
-        `${body.repository.full_name}`,
-        {},
-        () => {
-          repoChanged(body.repository.full_name, body.after);
-        });
-    } else {
-      git.cwd(`repos/${body.repository.full_name}`).pull(() => {
-        repoChanged(body.repository.full_name, body.after);
-      });
-    }
+    this.body = `Push to ${body.repository.full_name}\n`;
+    git.fetch(body.repository.full_name, body.after, licenses.discover, reporter);
   } else {
-    this.body = 'This endpoint only supports POST';
+    this.body = 'This endpoint only supports POST\n';
   }
 });
 
