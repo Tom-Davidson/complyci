f50140ffdaac30af4685b5e864a0465f2d11e0ac
Tom Davidson <tom@davidson.me.uk>
2017-07-27 21:40:09 +0100

    linting for mocha

--- test/reporters.js
+++ test/reporters.js
@@ -2,14 +2,14 @@ const assert = require('assert');
 const sinon = require('sinon');
 const reporters = require('../lib/reporters');
 
-describe('stdlib', function() {
+describe('stdlib', () => {
   it('dumps data to the console', () => {
     const spy = sinon.spy(console, 'log');
 
     reporters.stdout(
       '404-User/404-Project',
       '01234567890abcdefghijklmnopqrstuvwxyz',
-      {'libA': 'MIT', 'libB': 'BSD'}
+      { libA: 'MIT', libB: 'BSD' },
     );
 
     assert(spy.callCount, 3);
